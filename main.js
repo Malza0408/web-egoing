913d22ef6de7ef0ac0b2a1db0f3dee3587afb901
Malza0408 <82519572+Malza0408@users.noreply.github.com>
2022-01-12 14:46:01 +0900

    웹사이트 :)

---
+++ main.js
@@ -0,0 +1,8 @@
+const second_form = document.querySelector("#second-form");
+
+const lastSubmit = document.querySelector("#second-submit");
+
+second_form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(submit);
+});
